9e8ff04b3756bc47b488c552e4fbd7521127fcea
jingyangzhenren <97828377+jingyangzhenren@users.noreply.github.com>
2022-07-03 08:26:32 +0300

    refactoring

--- src/main/java/com/kongrentian/plugins/nexus/capability/CapabilityConfiguration.java
+++ src/main/java/com/kongrentian/plugins/nexus/capability/CapabilityConfiguration.java
@@ -18,18 +18,39 @@ public class CapabilityConfiguration extends CapabilityConfigurationSupport {
 
 
     public CapabilityConfiguration(Map<String, String> properties) {
-        apiUrl = properties.get(API_URL.propertyKey());
-        apiAuth = properties.get(API_TOKEN.propertyKey());
+        // cannot use just `get`, because capability can return null
+        // even if initial value is configured
+        // maybe it is because the old capability does not have those fields,
+        // and I have to recreate it
+        apiUrl = properties.getOrDefault(
+                API_URL.propertyKey(),
+                API_URL.defaultValue());
+        apiAuth = properties.getOrDefault(
+                API_TOKEN.propertyKey(),
+                API_TOKEN.defaultValue());
         apiTrustAllCertificates = Boolean.parseBoolean(
-                properties.get(API_TRUST_ALL_CERTIFICATES.propertyKey()));
-        userAgent = properties.get(USER_AGENT.propertyKey());
+                properties.getOrDefault(
+                        API_TRUST_ALL_CERTIFICATES.propertyKey(),
+                        API_TRUST_ALL_CERTIFICATES.defaultValue()));
+        userAgent = properties.getOrDefault(
+                USER_AGENT.propertyKey(),
+                USER_AGENT.defaultValue());
         connectionTimeout = Long.parseLong(
-                properties.get(CONNECTION_TIMEOUT.propertyKey()));
+                properties.getOrDefault(
+                        CONNECTION_TIMEOUT.propertyKey(),
+                        CONNECTION_TIMEOUT.defaultValue()));
         readTimeout = Long.parseLong(
-                properties.get(READ_TIMEOUT.propertyKey()));
+                properties.getOrDefault(
+                        READ_TIMEOUT.propertyKey(),
+                        READ_TIMEOUT.defaultValue()));
         writeTimeout = Long.parseLong(
-                properties.get(WRITE_TIMEOUT.propertyKey()));
-        scanInterval = Long.parseLong(properties.get(SCAN_INTERVAL.propertyKey()));
+                properties.getOrDefault(
+                        WRITE_TIMEOUT.propertyKey(),
+                        WRITE_TIMEOUT.defaultValue()));
+        scanInterval = Long.parseLong(
+                properties.getOrDefault(
+                        SCAN_INTERVAL.propertyKey(),
+                        SCAN_INTERVAL.defaultValue()));
     }
 
     public String getApiUrl() {
